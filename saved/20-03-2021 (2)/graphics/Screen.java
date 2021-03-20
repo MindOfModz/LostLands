d89f607bef5d7f8528bbcd4ae551059fc257a7d8
MindOfModz <65286575+MindOfModz@users.noreply.github.com>
2021-03-20 17:30:06 +0000

    Screen Java Class

---
+++ saved/20-03-2021 (2)/graphics/Screen.java
@@ -0,0 +1,31 @@
+package com.mindofmodz.lostlands.graphics;
+
+import java.util.Random;
+
+import com.mindofmodz.lostlands.Game;
+
+public class Screen extends Render {
+
+	private Render test;
+	private Render3D render;
+
+	public Screen(int width, int height) {
+		super(width, height);
+		Random random = new Random();
+		render = new Render3D(width, height);
+		test = new Render(256, 256);
+		for (int i = 0; i < 256 * 256; i++) {
+			test.pixels[i] = random.nextInt() * (random.nextInt(5) / 4);
+		}
+
+	}
+
+	public void render(Game game) {
+		for (int i = 0; i < width * height; i++) {
+			pixels[i] = 0;
+		}
+		
+		render.floor(game);
+		draw(render, 0, 0);
+	}
+}
