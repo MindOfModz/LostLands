b395445b109c4465fa825614127789d83c38961c
MindOfModz <65286575+MindOfModz@users.noreply.github.com>
2021-03-20 10:04:19 +0000

    Main Java Class

--- saved/20-03-2021/Main.java
+++ saved/20-03-2021/Main.java
@@ -1,1 +1,133 @@
+package com.mindofmodz.lostlands;
 
+import java.awt.Canvas;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
+
+import javax.swing.JFrame;
+
+import com.mindofmodz.lostlands.graphics.Render;
+import com.mindofmodz.lostlands.graphics.Screen;
+
+public class Display extends Canvas implements Runnable {
+	private static final long serialVersionUID = 1L;
+
+	public static final int WIDTH = 1280;
+	public static final int HEIGHT = 720;
+	public static final String TITLE = "LostLands Pre-Dev Pre-Alpha Test Build V0.1.1";
+
+	private Thread thread;
+	private Screen screen;
+	private BufferedImage img;
+	private Game game;
+	private boolean running = false;
+	private Render render;
+	private int[] pixels;
+
+	public Display() {
+		Dimension size = new Dimension(WIDTH, HEIGHT);
+		setPreferredSize(size);
+		setMinimumSize(size);
+		setMaximumSize(size);
+		screen = new Screen(WIDTH, HEIGHT);
+		game = new Game();
+		img = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+		pixels = ((DataBufferInt) img.getRaster().getDataBuffer()).getData();
+	}
+
+	private void start() {
+		if (running)
+			return;
+		running = true;
+		thread = new Thread(this);
+		thread.start();
+	}
+
+	public void stop() {
+		if (!running)
+			return;
+		running = false;
+		try {
+			thread.join();
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(0);
+		}
+	}
+
+	public void run() {
+		int frames = 0;
+		double unprocessedSeconds = 0;
+		long previousTime = System.nanoTime();
+		double secondsPerTick = 1 / 60.0;
+		int tickCount = 0;
+		boolean ticked = false;
+		
+		while (running) {
+			long currentTime = System.nanoTime();
+			long passedTime = currentTime - previousTime;
+			previousTime = currentTime;
+			unprocessedSeconds += passedTime / 1000000000.0;
+			
+			while (unprocessedSeconds > secondsPerTick) {
+				tick();
+				unprocessedSeconds -= secondsPerTick;
+				ticked = true;
+				tickCount++;
+				if (tickCount % 60 == 0) {
+					System.out.println(frames + " FPS");
+					previousTime += 1000;
+					frames = 0;
+				}
+			}
+			if (ticked) {
+				render();
+				frames++;
+			}
+			render();
+			frames++;
+		}
+	}
+
+	private void tick() {
+		game.tick();
+	}
+
+	private void render() {
+		BufferStrategy bs = this.getBufferStrategy();
+		if (bs == null) {
+			createBufferStrategy(3);
+			return;
+		}
+		
+		screen.render(game);
+		
+		for (int i = 0; i < WIDTH * HEIGHT; i++) {
+			pixels[i] = screen.pixels[i];
+		}
+		
+		Graphics g = bs.getDrawGraphics();
+		g.drawImage(img, 0, 0, WIDTH, HEIGHT, null);
+		g.dispose();
+		bs.show();
+	}
+
+	public static void main(String[] args) {
+		Display game = new Display();
+		JFrame frame = new JFrame();
+		frame.add(game);
+		frame.pack();
+		frame.setTitle(TITLE);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLocationRelativeTo(null);
+		frame.setAlwaysOnTop(true);
+		frame.setResizable(false);
+		frame.setVisible(true);
+
+		System.out.println("Running successfully...");
+		game.start();
+	}
+}
