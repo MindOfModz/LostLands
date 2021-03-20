f29a9ae494ad37fe642cd486fbbef02f61fcd153
MindOfModz <65286575+MindOfModz@users.noreply.github.com>
2021-03-20 17:28:33 +0000

    Game Java Class

---
+++ saved/20-03-2021 (2)/Game.java
@@ -0,0 +1,27 @@
+package com.mindofmodz.lostlands;
+
+import java.awt.event.KeyEvent;
+
+import com.mindofmodz.lostlands.input.Controller;
+
+public class Game {
+	public int time;
+	public Controller controls;
+	
+	public Game() {
+		controls = new Controller();
+	}
+	
+	public void tick(boolean[] key) {
+		time++;
+		boolean forward = key[KeyEvent.VK_W];
+		boolean left = key[KeyEvent.VK_A];
+		boolean right = key[KeyEvent.VK_D];
+		boolean back = key[KeyEvent.VK_S];
+		boolean turnLeft = key[KeyEvent.VK_LEFT];
+		boolean turnRight = key[KeyEvent.VK_RIGHT];
+		
+		controls.tick(forward, back, left, right, turnLeft, turnRight);
+	}
+
+}
