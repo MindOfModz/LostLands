60e539c835a448f6d44d853399e4b73ce9b640da
MindOfModz <65286575+MindOfModz@users.noreply.github.com>
2021-03-20 17:29:08 +0000

    Render Java Class

---
+++ saved/20-03-2021 (2)/graphics/Render.java
@@ -0,0 +1,36 @@
+package com.mindofmodz.lostlands.graphics;
+
+import com.mindofmodz.lostlands.Display;
+
+public class Render {
+	public final int width;
+	public final int height;
+	public final int[] pixels;
+
+	public Render(int width, int height) {
+		this.width = width;
+		this.height = height;
+		pixels = new int[width * height];
+	}
+
+	public void draw(Render render, int xOffset, int yOffset) {
+		for (int y = 0; y < render.height; y++) {
+			int yPix = y + yOffset;
+			if (yPix < 0 || yPix >= Display.HEIGHT) {
+				continue;
+			}
+
+			for (int x = 0; x < render.width; x++) {
+				int xPix = x + xOffset;
+				if (xPix < 0 || xPix >= Display.WIDTH) {
+					continue;
+				}
+
+				int alpha = render.pixels[x + y * render.width];
+				if (alpha > 0) {
+					pixels[xPix + yPix * width] = alpha;
+				}
+			}
+		}
+	}
+}
